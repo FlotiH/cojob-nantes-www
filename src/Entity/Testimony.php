e071562542cde2c4b77e9438388e59911ab3f0ca
FlotiH <tiflo.hamard@wanadoo.fr>
2026-03-26 15:10:49 +0100

    Helloasso adh 2026 form + new donate form + hide twitter and airtable

---
+++ src/Entity/Testimony.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Blameable\Traits\BlameableEntity;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'testimony')]
+#[Gedmo\SoftDeleteable(fieldName: 'deletedAt')]
+class Testimony
+{
+    use BlameableEntity;
+    use SoftDeleteableEntity;
+    use TimestampableEntity;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 63)]
+    #[Assert\Length(max: 63)]
+    #[Assert\NotBlank]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\Length(max: 255)]
+    #[Assert\NotBlank]
+    private $subtitle;
+
+    #[ORM\Column(type: 'text')]
+    #[Assert\NotBlank]
+    private $content;
+
+    #[ORM\Column(type: 'boolean')]
+    private $promo = true;
+
+    #[ORM\Column(type: 'boolean')]
+    private $requiredDisplaying = false;
+
+    public function __toString()
+    {
+        return (string) $this->name;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSubtitle()
+    {
+        return $this->subtitle;
+    }
+
+    public function setSubtitle($subtitle)
+    {
+        $this->subtitle = $subtitle;
+
+        return $this;
+    }
+
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function isPromo()
+    {
+        return $this->promo;
+    }
+
+    public function setPromo($promo)
+    {
+        $this->promo = $promo;
+
+        return $this;
+    }
+
+    public function isRequiredDisplaying()
+    {
+        return $this->requiredDisplaying;
+    }
+
+    public function setRequiredDisplaying($requiredDisplaying)
+    {
+        $this->requiredDisplaying = $requiredDisplaying;
+
+        return $this;
+    }
+}
